4f2e96fb493579d98cdb1b8143c9e32a0bcda727
Brian Liotti <bdliotti@gmail.com>
2018-12-22 14:51:17 -0500

    done page

--- src/components/NewItem.js
+++ src/components/NewItem.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import uniqueId from 'lodash/uniqueId';
+import React, { Component } from 'react'
+import uniqueId from 'lodash/uniqueId'
 
-import './NewItem.css';
+import './NewItem.css'
 
 class NewItem extends Component {
-  state = { value: '' };
+  state = { value: '' }
 
   handleChange = event => {
-    // Do something when the state of this input changes.
-  };
+    const value = event.target.value
+    this.setState({ value })
+  }
 
   handleSubmit = event => {
-    const { onSubmit } = this.props;
-    const { value } = this.state;
+    const { onSubmit } = this.props
+    const { value } = this.state
 
-    event.preventDefault();
+    event.preventDefault()
 
     // Do something when a new value is submitted.
-
+    onSubmit({ value, id: uniqueId(), packed: false })
+    this.setState({ value: '' })
     // Reset the state of the component.
-  };
+  }
 
   render() {
-    const { value } = this.state;
+    const { value } = this.state
 
     return (
       <form className="NewItem" onSubmit={this.handleSubmit}>
@@ -34,8 +36,8 @@ class NewItem extends Component {
         />
         <input className="NewItem-submit button" type="submit" />
       </form>
-    );
+    )
   }
 }
 
-export default NewItem;
+export default NewItem
